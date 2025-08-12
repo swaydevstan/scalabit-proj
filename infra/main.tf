a4287b65c423d8cdac50bfd30ef410df21e7d015
Stanley Godspower <stan>
2025-08-12 14:49:43 +0100

    Added workflow file

--- infra/main.tf
+++ infra/main.tf
@@ -16,3 +16,15 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+
+resource "google_project_service" "enabled_services" {
+  for_each = toset([
+    "compute.googleapis.com",
+    "iap.googleapis.com",
+    "cloudresourcemanager.googleapis.com"
+  ])
+  project = var.project_id
+  service = each.key
+  disable_on_destroy = false
+}
