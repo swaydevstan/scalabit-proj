fa9602438fe94f16b6b3d617a1b6698515f0ed23
Stanley Godspower <stan>
2025-08-13 01:42:31 +0100

    Added more network resources

--- infra/main.tf
+++ infra/main.tf
@@ -28,3 +28,61 @@ resource "google_compute_subnetwork" "subnet" {
   region        = var.region
   network       = google_compute_network.vpc_network.id
 }
+
+resource "google_compute_router" "router" {
+  name    = "${var.project_name}-router"
+  region  = var.region
+  network = google_compute_network.vpc_network.id
+}
+resource "google_compute_router_nat" "nat" {
+  name                               = "${var.project_name}-nat"
+  router                             = google_compute_router.router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+resource "google_compute_firewall" "allow_iap" {
+  name    = "allow-iap"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443", "6443"]
+  }
+  source_ranges = ["35.235.240.0/20"]
+  target_tags   = ["${var.project_name}-node"]
+}
+resource "google_compute_firewall" "allow_internal" {
+  name    = "allow-internal"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+  }
+  allow {
+    protocol = "udp"
+  }
+  source_ranges = ["10.0.0.0/24"]
+  target_tags   = ["${var.project_name}-node"]
+}
+
+resource "google_artifact_registry_repository" "repo" {
+  location      = var.region
+  repository_id = "${var.project_name}-repo"
+  description   = "Image repository for Scalabit Project"
+  format        = "DOCKER"
+}
+resource "google_service_account" "scalabit_sa" {
+  account_id   = "${var.project_name}-sa"
+  display_name = "Scalabit k3s Node Service Account"
+}
+
+resource "google_project_iam_member" "scalabit_sa_bindings" {
+  for_each = toset([
+    "roles/artifactregistry.reader",
+    "roles/logging.logWriter",
+    "roles/monitoring.metricWriter"
+  ])
+
+  project = var.project_id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.scalabit_sa.email}"
+}
